3f8e3622a0716336ac79f294f503658f5ce28a4c
Elie Cohen <ec92009@gmail.com>
2025-02-24 20:17:37 +0100

    Initial Commit

---
+++ AIPhotoKey/AIPhotoKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIPhotoKeyApp.swift
+//  AIPhotoKey
+//
+//  Created by Elie Cohen on 2025-02-24.
+//
+
+import SwiftUI
+
+@main
+struct AIPhotoKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIPhotoKeyTests/AIPhotoKeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIPhotoKeyTests.swift
+//  AIPhotoKeyTests
+//
+//  Created by Elie Cohen on 2025-02-24.
+//
+
+import Testing
+@testable import AIPhotoKey
+
+struct AIPhotoKeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
